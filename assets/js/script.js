28069830854edf25dc08df532fe708b8ddba4c3d
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-04-08 18:03:40 -0700

    draggable complete but replicating tasks needs to be debugged

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,7 +165,80 @@ $(".list-group").on("click", "span", function() {
     
 });
 
+// adding the sortable functionality / sortable() method turned every element with the list-group class into sortable items / the connectWith property when linked these sortable lists with any other lists that have the same class
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    // console.log("activate", this);
+  },
+  deactivate: function(event) {
+    // console.log("deactivate", this);
+  },
+  over: function(event) {
+    // console.log("over", event.target);
+  },
+  out: function(event) {
+    // console.log("out", event.target);
+  },
+  update: function(event) {
+    // array to store the task data in
+    var tempArr = [];
+    // loop over the current set of children elements in sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
 
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+    //console.log(tempArr);
+    //console.log($(this).children());
+    // children () method returns an array of the list element's children
+    //the each() method will run a callback function for every item/element in the array
+    //$(this) refers to the child element at that index - research this a little in the docs
+  }
+});
+
+// above adding some event listeners / research each of these in the docs / the active and deactivate events trigger once for all connected lists as soon as dragging starts and stops / the over and out events trigger when a dragged item enters or leaves a connected list / the updated event triggers when the contents of a list have changed
+
+// making the trash bin operational
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+    // remove() will remove that element from the DOM entirely
+  },
+  over: function(event, ui) {
+    //console.log("over");
+  },
+  out: function(event, ui) {
+    //console.log("out");
+  }
+});
 
 
 
