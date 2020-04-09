34b5a102bb4ec398d74829ac0053a90bcb6d76d8
DJS <davidstahl@Davids-MacBook-Pro.local>
2020-04-09 16:33:29 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -224,14 +224,20 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event) {
     // console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag")
     // console.log("deactivate", this);
   },
   over: function(event) {
+    $(event.target).addClass("dropover-active");
     // console.log("over", event.target);
   },
   out: function(event) {
+    $(event.target).removeClass("dropover-active");
     // console.log("out", event.target);
   },
   update: function(event) {
@@ -321,12 +327,15 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $(".bottom-trash").removeClass("bottom-trash-active");
     // remove() will remove that element from the DOM entirely
   },
   over: function(event, ui) {
     //console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
+    $(".bottom-trash").removeClass("bottom-trash-active");
     //console.log("out");
   }
 });
@@ -352,7 +361,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
